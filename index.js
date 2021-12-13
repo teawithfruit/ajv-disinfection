abe05ae9e2c0265f2d1df71da2b406a08459fd19
teawithfruit <teawithfruit@gmail.com>
2021-12-14 00:02:27 +0100

    keyword update

--- index.js
+++ index.js
@@ -6,34 +6,28 @@ const ajvDisinfection = (ajv, extraDisinfection) => {
 		...extraDisinfection
 	};
 
-  ajv.addKeyword('disinfect', {
+  ajv.addKeyword({
+		keyword: 'disinfect',
 		modifying: true,
 		compile: function compile(schema) {
 			let disinfect;
 
-			if(typeof schema === 'string') {
-				disinfect = extendedDisinfection[schema];
-			}
+			if(typeof schema === 'string') disinfect = extendedDisinfection[schema];
+			if(typeof schema === 'object') disinfect = extendedDisinfection[schema.name];
+			if(typeof schema === 'function') disinfect = schema;
 
-			if(typeof schema === 'object') {
-				disinfect = extendedDisinfection[schema.name];
-			}
-
-			if(typeof schema === 'function') {
-				disinfect = schema;
-			}
-
-			if(!disinfect) {
-				throw new TypeError('Unknown disinfection');
-			}
+			if(!disinfect) throw new TypeError('Unknown disinfection');
 
 			return (data, currentDataPath, parentDataObject, propertyName) => {
-				if(!propertyName && propertyName !== 0) throw new TypeError('Data must be a property of an object');
+				if (!currentDataPath && !currentDataPath.parentDataProperty && currentDataPath.parentDataProperty !== 0) throw new TypeError('Data must be a property of an object');
 
-				if(typeof schema === 'object') {
-					parentDataObject[propertyName] = disinfect(data, schema);
-				} else {
-					parentDataObject[propertyName] = disinfect(data);
+				if(data) {
+					if(typeof schema === 'object') {
+						currentDataPath.parentData[currentDataPath.parentDataProperty] = disinfect(data, schema);
+					} else {
+						console.log('blub');
+						currentDataPath.parentData[currentDataPath.parentDataProperty] = disinfect(data);
+					}
 				}
         
 				return true;
